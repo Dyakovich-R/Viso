ac44457fb8426cea14075328ae3c61cd9a7e0da1
Roman <rdyakovich@gmail.com>
2024-06-05 14:23:22 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Maps } from './components/Maps';
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello word</h1>
+      <Maps />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Maps.tsx
@@ -0,0 +1,15 @@
+//import React from 'react';
+
+//interface IMaps {
+//  markers: Array<{ id: string; lat: number; lng: number }>;
+//  onAddMarker: (lat: number, lng: number) => void;
+//  onRemoveMarker: (id: string) => void;
+//}
+
+export const Maps = ({
+  //markers,
+  //onAddMarker,
+  //onRemoveMarker,
+}) => {
+  return <div>Maps</div>;
+};
