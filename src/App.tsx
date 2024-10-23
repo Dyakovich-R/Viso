06162037a7274cc123a0c928dd889af827cb18b1
Roman <rdyakovich@gmail.com>
2024-10-23 14:08:23 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Button } from './components/ui/button';
+
+export const App = () => {
+
+  return (
+    <>
+    <h1>hello
+      <Button>Button</Button>
+    </h1>
+    </>
+  );
+};
